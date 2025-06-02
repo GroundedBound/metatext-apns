8103b05e8ad331d489f69fd31273af8218b91d84
JunyuKuang <122197178+GroundedBound@users.noreply.github.com>
2025-06-02 20:47:42 +0900

    Update app.rb

--- app.rb
+++ app.rb
@@ -9,6 +9,7 @@ configure :production do
 end
 
 set :views, File.expand_path('../views', __FILE__)
+set :public, File.expand_path('../public', __FILE__)
 
 require 'dotenv/load' if ENV['RACK_ENV'] == 'development'
 
