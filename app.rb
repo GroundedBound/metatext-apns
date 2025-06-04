bb3cfa8ab923bfa407f176978ebaacda22ffeead
JunyuKuang <122197178+GroundedBound@users.noreply.github.com>
2025-06-04 22:21:37 +0900

    Update app.rb

--- app.rb
+++ app.rb
@@ -42,34 +42,22 @@ post '/push/:app_id/:device_token/:user_id' do
   
   salt = nil
   dh = nil
-  encoded_payload = nil
+  ciphertext = nil
   
   if content_encoding == 'aes128gcm'
     # AES128GCM payload structure:
     # [salt (16)][rs (4)][sender pubkey (65)][ciphertext+tag]
     raw = body_data.b
-    salt = raw[0...16]
-    rs = raw[16...20]                  # usually 4096 (not used here)
-    dh = raw[20...85]                  # 65-byte uncompressed EC public key
-    ciphertext = raw[85..]
-    
-    encoded_payload = {
-      i: params[:user_id],
-      m: Base64.urlsafe_encode64(ciphertext),
-      s: Base64.urlsafe_encode64(salt),
-      k: Base64.urlsafe_encode64(dh),
-      e: 'aesgcm'
-    }
+    if raw.bytesize < 85
+        halt 400, 'Malformed payload'
+    end
+    salt = Base64.urlsafe_encode64(raw[0...16])
+    dh = Base64.urlsafe_encode64(raw[20...85]) # 65-byte uncompressed EC public key
+    ciphertext = Base64.urlsafe_encode64(raw[85..])
   else
     salt = request.env['HTTP_ENCRYPTION'].to_s.split('salt=').last&.split(';')&.first
-    dh   = request.env['HTTP_CRYPTO_KEY'].to_s.split('dh=').last&.split(';')&.first
-    encoded_payload = {
-      i: params[:user_id],
-      m: Base64.urlsafe_encode64(body_data),
-      s: salt,
-      k: dh,
-      e: 'aesgcm'
-    }
+    dh = request.env['HTTP_CRYPTO_KEY'].to_s.split('dh=').last&.split(';')&.first
+    ciphertext = Base64.urlsafe_encode64(body_data)
   end
   
   notification = Apnotic::Notification.new(params[:device_token])
@@ -77,7 +65,13 @@ post '/push/:app_id/:device_token/:user_id' do
   notification.alert = { 'loc-key' => 'apns-default-message' }
   notification.mutable_content = true
   notification.interruption_level = "passive"
-  notification.custom_payload = encoded_payload
+  
+  notification.custom_payload = {
+    i: params[:user_id],
+    m: ciphertext,
+    s: salt,
+    k: dh
+  }
   
   if params[:sandbox] == 'true'
     push = SANDBOX_CONNECTION.prepare_push(notification)
